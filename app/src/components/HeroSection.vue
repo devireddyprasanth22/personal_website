5c595cf679016b02e68cf4ec848e1abe399fc6b4
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-01-08 14:58:33 +1100

    working on hero section

--- app/src/components/HeroSection.vue
+++ app/src/components/HeroSection.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="max-w-screen-xl px-4 py-8 mx-auto">
-        Hi
+
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi</h1>
+        <!-- Header -->
+		<p class="flex flex-col select-none font-extrabold text-main text-lg leading-none h-[10vw] ssm:h-[50px] mt-[5px]">
+			<span class="ssm:text-[40px] select-none text-xl text-main ">
+				I'm 
+				<span class="hover:text-accent">Prasanth.</span>
+			</span>
+		</p>
     </div>
 </template>
