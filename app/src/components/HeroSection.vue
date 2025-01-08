fb104de30fd238592f6af4218ec0fede76398196
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-01-08 15:24:29 +1100

    hero intro

--- app/src/components/HeroSection.vue
+++ app/src/components/HeroSection.vue
@@ -1,13 +1,20 @@
 <template>
+    <section class="py-32">
     <div class="max-w-screen-xl px-4 py-8 mx-auto">
 
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi</h1>
-        <!-- Header -->
-		<p class="flex flex-col select-none font-extrabold text-main text-lg leading-none h-[10vw] ssm:h-[50px] mt-[5px]">
-			<span class="ssm:text-[40px] select-none text-xl text-main ">
-				I'm 
-				<span class="hover:text-accent">Prasanth.</span>
-			</span>
-		</p>
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Hi</h1>
+            <h2 class="max-w-xl mb-2 text-2xl tracking-tight leading-none md:text-5xl xl:text-4xl dark:text-white">I am Prasanth</h2>
+            <span class="ssm:text-[40px] select-none text-xl text-main ">
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A software engineer dedicated to exploring different technologies and applying learning in practical environments</p>
+        </span>
+        </div>
+        </div>
     </div>
+</section>
 </template>
+<script>
+</script>
+<style scoped>
+</style>
