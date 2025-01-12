4ee7a05731da2026294eb7850402ba83b3c9a70d
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-01-12 16:12:58 +1100

    change navbar to home, interests, projects and contact; need to refine Interests

--- app/src/components/Interests.vue
+++ app/src/components/Interests.vue
@@ -1,12 +1,19 @@
 <template>
-    <section class="mt-12">
+    <section class="mt-16">
         <div class = "text-center">
             <div class="px-48 py-3 flex items-center text-sm text-gray-800 before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-white dark:before:border-neutral-600 dark:after:border-neutral-600">
                 <h2 class="max-w-xl mb-2 text-2xl tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">Interests</h2>
             </div>
-            <p class=" px-48 mt-4 text-lg text-gray-600 md:text-xl xl:text-2xl dark:text-gray-400 animate-fade-in-delayed">
-        Beyond my professional pursuits, I love staying active and this takes the form of bouldering, going to the gym and hiking. When it is time to unwind, I find myself either reading books (currently reading ...) or enjoying a game of pool. More recently, my love for music has led me to explore the timeless charm of record players and vinyls.
-      </p>
+            <p class="px-48 mt-4 text-lg text-gray-600 md:text-xl xl:text-2xl dark:text-gray-400">
+    Beyond my professional pursuits, I love staying active and this takes the form of bouldering 
+    <img class="inline-block align-middle" width="32" height="32" src="https://img.icons8.com/emoji/48/man-climbing.png" alt="man-climbing"/>, 
+    going to the gym <img class="inline-block align-middle" width="32" height="32" src="https://img.icons8.com/emoji/50/man-lifting-weights.png" alt="man-lifting-weights"/> 
+    and hiking <img class="inline-block align-middle" width="32" height="32" src="https://img.icons8.com/office/40/trekking.png" alt="trekking"/>. When it is time to unwind, I find myself either reading books
+    <img class="inline-block align-middle" width="32" height="32" src="https://img.icons8.com/color/50/open-book.png" alt="open-book"/>
+     (currently reading ...) or enjoying a game of pool <img class="inline-block align-middle" width="32" height="32" src="https://img.icons8.com/emoji/50/pool-8-ball.png" alt="pool-8-ball"/>. 
+    More recently, my love for music has led me to explore the timeless charm of record players and vinyls.
+</p>
+
     
         </div>
 
--- app/src/components/NavBar.vue
+++ app/src/components/NavBar.vue
@@ -3,8 +3,8 @@
       <div class="px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
               <div class="flex items-baseline space-x-4 ml-auto">
+                <a href="#" class="text-gray-300 hover:bg-green-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
                 <a href="#" class="text-gray-300 hover:bg-green-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Interests</a>
-                <a href="#" class="text-gray-300 hover:bg-green-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Experience</a>
                 <a href="#" class="text-gray-300 hover:bg-green-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
                 <a href="#" class="text-gray-300 hover:bg-green-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
             </div>
