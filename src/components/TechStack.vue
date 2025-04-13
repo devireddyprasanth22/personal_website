d0808a88d3d5ab2adb5a9dcd10b2f2f7fc755d6e
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-04-13 15:50:47 +0800

    updated colours for dark and light mode

--- src/components/TechStack.vue
+++ src/components/TechStack.vue
@@ -3,20 +3,24 @@
         <section class="grid gap-1" style="gap: 2rem">
             <article class="container__main flex-column gap-1">
                 <div class="container__techstacks">
-                    <h2 class="font-bold lg:text-2xl dark:text-gray-100 text-gray-800">
-                        Tech and tools I use
-                    </h2>
-                    <div class="container__techstacks flex gap-1 sm: flex-wrap">
-                        <img
-                            v-for="techstack in techStack"
-                            :key="techstack.name"
-                            :src="techstack.icon"
-                            :alt="techstack.name"
-                            :title="techstack.name"
-                            class="techstack__icon"
-                        />
-                    </div>
-                </div>
+    <h2 class="font-bold lg:text-2xl dark:text-gray-100 light:text-gray-300 text-center md:text-left">
+        Tech and tools I use
+    </h2>
+    <div
+        class="container__techstacks flex gap-1
+               flex-wrap justify-center
+               md:justify-start md:flex-nowrap"
+    >
+        <img
+            v-for="techstack in techStack"
+            :key="techstack.name"
+            :src="techstack.icon"
+            :alt="techstack.name"
+            :title="techstack.name"
+            class="techstack__icon"
+        />
+    </div>
+</div>
                 </article>
                 </section>
  </div>
